e246f7341a58f12ad6280ab7937781f1725b9794
NoF0rte <nof0rte@users.noreply.github.com>
2025-02-19 13:48:02 -0700

    Added a way to add groups just on the command line, without any files

--- cmd/groups_add.go
+++ cmd/groups_add.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/NoF0rte/gophish-client/api/models"
+	"github.com/gocarina/gocsv"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +17,9 @@ var groupsAddCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		dir, _ := cmd.Flags().GetString("dir")
 		groupPaths, _ := cmd.Flags().GetStringSlice("groups")
+		name, _ := cmd.Flags().GetString("name")
+		targetsFile, _ := cmd.Flags().GetString("targets-file")
+		targetsArr, _ := cmd.Flags().GetStringArray("targets")
 
 		if dir != "" {
 			files, err := filepath.Glob(filepath.Join(dir, "*.yaml"))
@@ -28,11 +33,34 @@ var groupsAddCmd = &cobra.Command{
 			groupPaths = append(groupPaths, files...)
 		}
 
-		for _, g := range groupPaths {
-			group, err := models.GroupFromFile(g, variables)
-			checkError(err)
+		if len(groupPaths) != 0 {
+			for _, g := range groupPaths {
+				group, err := models.GroupFromFile(g, variables)
+				checkError(err)
 
-			fmt.Printf("[+] Adding group \"%s\"\n", group.Name)
+				fmt.Printf("[+] Adding group \"%s\"\n", group.Name)
+
+				_, err = client.CreateGroup(group)
+				checkError(err)
+			}
+		} else {
+			var err error
+			var targets []*models.Target
+			if targetsFile != "" {
+				targets, err = models.TargetsFromFile(targetsFile)
+				checkError(err)
+			} else if len(targetsArr) != 0 {
+				reader := strings.NewReader(strings.Join(targetsArr, "\n"))
+				err = gocsv.UnmarshalWithoutHeaders(reader, &targets)
+				if err != nil {
+					checkError(fmt.Errorf("failed to parse targets: %v", err))
+				}
+			}
+
+			group := &models.Group{
+				Name:    name,
+				Targets: targets,
+			}
 
 			_, err = client.CreateGroup(group)
 			checkError(err)
@@ -44,5 +72,8 @@ func init() {
 	groupsCmd.AddCommand(groupsAddCmd)
 
 	groupsAddCmd.Flags().StringP("dir", "d", "", "Directory containing the groups to add. Both .yaml and .yml files will be used.")
-	groupsAddCmd.Flags().StringSliceP("groups", "g", []string{}, "The paths of the groups to add. Specify multiple times to add more groups.")
+	groupsAddCmd.Flags().StringSliceP("groups", "g", []string{}, "The paths of the group files to add. Specify multiple times to add more groups.")
+	groupsAddCmd.Flags().StringP("name", "n", "", "Name of the group to add")
+	groupsAddCmd.Flags().String("targets-file", "", "The path to the file containing the targets. Can be CSV or YAML")
+	groupsAddCmd.Flags().StringArrayP("targets", "t", []string{}, "Comma separated value representing the target. Specify multiple times for more targets. Format: FirstName,LastName,Email,Position")
 }
